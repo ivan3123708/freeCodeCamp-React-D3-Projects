3d76d805537a31b74bd773f66cad892b7765b552
Ivan <ivan3123708@gmail.com>
2017-12-16 14:35:05 +0100

    Added server

---
+++ Camper Leaderboard/server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const buildPath = path.join(__dirname, '..', 'build');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(buildPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is running...');
+});
