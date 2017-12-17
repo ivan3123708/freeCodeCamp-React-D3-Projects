295444368a16b79f184011bcab8ae75d0f7981d5
Ivan <ivan3123708@gmail.com>
2017-12-17 03:11:04 +0100

    Added production build script and server

--- Recipe Box/webpack.config.js
+++ Recipe Box/webpack.config.js
@@ -1,38 +1,41 @@
 const path = require('path');
 
-const config = {
+module.exports = (env) => {
+
+  const isProduction = env === 'production';
+
+  return {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js'
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env', 'react'],
-                        plugins: ['transform-class-properties']
-                    }
-                }
-            },
-            {
-                test: /\.s?css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-              test: /\.(png|jpg)$/,
-              loader: 'url-loader'
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env', 'react'],
+              plugins: ['transform-class-properties']
             }
-        ]
+          }
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: 'url-loader'
+        }
+      ]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+      contentBase: path.join(__dirname, 'public')
     }
+  }
 }
-
-module.exports = config;
