338c0e67fcabcf1fb11c46398f86927b71255ff8
Ivan <ivan3123708@gmail.com>
2017-12-17 02:06:12 +0100

    Fixed image spacing

--- Recipe Box/src/components/RecipeBox.js
+++ Recipe Box/src/components/RecipeBox.js
@@ -9,11 +9,31 @@ class RecipeBox extends React.Component {
 
     state = {
         recipes: [
-            {
-                title: 'Black Magic Cake',
-                image: 'http://img.taste.com.au/9isesBer/taste/2016/11/caramello-cake-105070-1.jpeg',
-                ingredients: '1 3/4 cups all-purpose flour, 2 cups white sugar, 3/4 cup unsweetened cocoa powder, 2 teaspoons baking soda, 1 teaspoon baking powder, 1 teaspoon salt, 2 eggs, 1 cup strong brewed coffee, 1 cup buttermilk, 1/2 cup vegetable oil, 1 teaspoon vanilla extract'
-            }
+          {
+            title: 'Sesame Beef',
+            image: 'http://images.media-allrecipes.com/userphotos/720x405/858484.jpg',
+            ingredients: '1 pound round steak, 4 tablespoons soy sauce, 4 tablespoons white sugar, 4 tablespoons vegetable oil, 2 cloves garlic minced, 2 green onions chopped, 2 tablespoons sesame seeds'
+          },
+          {
+            title: 'Pasta Pomodoro',
+            image: 'http://images.media-allrecipes.com/userphotos/720x405/632125.jpg',
+            ingredients: '1 (16 ounce) package angel hair pasta, 1/4 cup olive oil 1/2 onion chopped, 4 cloves garlic minced, 2 cups roma (plum) tomatoes diced, 2 tablespoons balsamic vinegar, 1 (10.75 ounce) can low-sodium chicken broth, crushed red pepper to taste, freshly ground black pepper to taste, 2 tablespoons chopped fresh basil, 1/4 cup grated Parmesan cheese'
+          },
+          {
+            title: 'Black Magic Cake',
+            image: 'http://images.media-allrecipes.com/userphotos/720x405/1117116.jpg',
+            ingredients: '1 3/4 cups all-purpose flour, 2 cups white sugar, 3/4 cup unsweetened cocoa powder, 2 teaspoons baking soda, 1 teaspoon baking powder, 1 teaspoon salt, 2 eggs, 1 cup strong brewed coffee, 1 cup buttermilk, 1/2 cup vegetable oil, 1 teaspoon vanilla extract'
+          },
+          {
+            title: 'Chicken Pizza',
+            image: 'http://images.media-allrecipes.com/userphotos/720x405/2585431.jpg',
+            ingredients: '1 tablespoon sesame oil, 1 skinless boneless chicken breast half, 1/4 cup barbeque sauce divided, 1/2 cup marinara sauce, 1 (16 ounce) package pre-baked pizza crust, 1 cup shredded mozzarella cheese, 1/3 cup thinly sliced red onion, 2 tablespoons chopped fresh cilantro'
+          },
+          {
+            title: 'Banana Ice Cream',
+            image: 'http://images.media-allrecipes.com/userphotos/720x405/788007.jpg',
+            ingredients: '2 1/2 cups half-and-half, 8 egg yolks beaten, 1 cup white sugar, 2 1/2 cups heavy whipping cream, 2 bananas, 3/4 cup half-and-half, 1 cup walnuts'
+          }
         ],
         selected: undefined,
         addModal: false,
